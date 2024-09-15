bd800e154a6b90ec106a40e24102ebe25a9645cb
Gaurav Yadav <yadavgaurv2003@gmail.com>
2024-09-15 22:33:11 +0530

    Set Images and Titles

--- inetrface.py
+++ inetrface.py
@@ -10,7 +10,6 @@ img= PhotoImage(file=r"D:\Downloads\n581fpen1.png")
 l2=Label(df,image=img)
 l2.place(x=620,y=75)
 
-
 wind_dir= PhotoImage(file=r"D:\Downloads\sj9carua.png")
 wind_l1=Label(df,image=wind_dir)
 wind_l1.place(x=1060,y=665)
@@ -39,6 +38,15 @@ wind_info= PhotoImage(file=r"D:\Downloads\apa3a5ab1.png")
 wind_l6=Label(df,image=wind_info)
 wind_l6.place(x=1190,y=300)
 
+humidity_img= PhotoImage(file=r"D:\Downloads\dew.png")
+humidity_l7=Label(df,image=humidity_img)
+humidity_l7.place(x=1370,y=570)
+
+weather_info= PhotoImage(file=r"D:\Downloads\5zzdx0mk.png")
+waether_l8=Label(df,image=weather_info)
+waether_l8.place(x=1360,y=670)
+
+
 v=StringVar()
 
 def wheather_status(event=None):
@@ -53,6 +61,8 @@ def wheather_status(event=None):
     show_wa.config(text=str(data["current"]["wind_degree"]))
     show_wd.config(text=str(data["current"]["wind_dir"]))
     show_dt.config(text=str(data["current"]["last_updated"]))
+    show_country.config(text=str(data["location"]["country"]))
+    show_humidity.config(text=str(data["current"]["humidity"]))
 
 
     condition_text = data["current"]["condition"]["text"]
@@ -81,66 +91,77 @@ txt2.place(x=1115,y=235)
 txt3=Label(df,font=("Calibari",11),text="* Note : Click on Submit / Press Enter ",fg="Red")
 txt3.place(x=630,y=458)
 
-Btn= Button(df, font=("Arial Black",12),text="Submit",fg="orange",bg="skyblue",command=wheather_status)
+Btn= Button(df, font=("Arial Black",12),text="Submit",fg="darkorange",bg="skyblue",command=wheather_status)
 Btn.place(x=720,y=415)
 
 temp_r=Label(df,font=("Arial Black",15),text="Region",fg="black")
 temp_r.place(x=360,y=490)
 
-show_r=Label(df,font=("Arial",15),text=" ",fg="red")
-show_r.place(x=370,y=530)
+show_r=Label(df,font=("Arial",15),fg="teal",)
+show_r.place(x=360,y=530)
 
 temp_f=Label(df,font=("Arial Black",15),text="Tempreture in Fahrenheit (°F)",fg="black")
 temp_f.place(x=600,y=490)
 
-show_f=Label(df,font=("Arial",15),text=" ",fg="red")
+show_f=Label(df,font=("Arial",15),text=" ",fg="teal")
 show_f.place(x=740,y=530)
 
 temp_c=Label(df,font=("Arial Black",15),text="Tempreture in Celsius (°C)",fg="black")
 temp_c.place(x=970,y=490)
 
-show_c=Label(df,font=("Arial",15),text=" ",fg="red")
+show_c=Label(df,font=("Arial",15),text=" ",fg="teal")
 show_c.place(x=1090,y=530)
 
 ws=Label(df,font=("Arial Black",15),text="Wind Speed (Kph)",fg="black")
 ws.place(x=320,y=580)
 
-show_ws=Label(df,font=("Din",15),text=" ",fg="green")
+show_ws=Label(df,font=("Din",15),text=" ",fg="darkgreen")
 show_ws.place(x=400,y=620)
 
 w_a=Label(df,font=("Arial Black",15),text="Wind Angle (°)",fg="black")
 w_a.place(x=690,y=580)
 
-show_wa=Label(df,font=("Din",15),text=" ",fg="green")
+show_wa=Label(df,font=("Din",15),text=" ",fg="darkgreen")
 show_wa.place(x=740,y=620)
 
 w_d=Label(df,font=("Arial Black",15),text="Wind Direction",fg="black")
 w_d.place(x=1035,y=580)
 
-show_wd=Label(df,font=("Din",15),text=" ",fg="green")
+show_wd=Label(df,font=("Din",15),text=" ",fg="darkgreen")
 show_wd.place(x=1095,y=620)
 
+country=Label(df,font=("Arial Black",15),text="Country",fg="black")
+country.place(x=140,y=490)
+
+show_country=Label(df,font=("Arial",15),fg="red")
+show_country.place(x=150,y=530)
+
+temp_h=Label(df,font=("Arial Black",15),text="Humidity (%)",fg="black")
+temp_h.place(x=1340,y=490)
+
+show_humidity=Label(df,font=("Arial",15),text=" ",fg="red")
+show_humidity.place(x=1390,y=530)
 
 blank_body=Label(df,bg="deepskyblue",width=600,height=5)
 blank_body.pack()
 
-blank_body_text=Label(df,font=("Arial Black",20),text="WEATHER FORECASTING APP",width=60,justify="center",bg="deepskyblue")
-blank_body_text.place(x=230,y=16)
+blank_body_text=Label(df,font=("Arial Black",23),text="WEATHER FORECASTING APP",width=60,justify="center",bg="deepskyblue")
+blank_body_text.place(x=135,y=16)
 
-dt=Label(df,font=("Din",12,"bold"),text="Last Updated : ",fg="black",bg="deepskyblue")
-dt.place(x=1260,y=30)
+dt=Label(df,font=("Din",12),text="Last Updated : ",fg="black",bg="deepskyblue")
+dt.place(x=1260,y=10)
 
-show_dt=Label(df,font=("Din",12,"bold"),text=" ",fg="red",bg="deepskyblue")
-show_dt.place(x=1380,y=30)
+show_dt=Label(df,font=("Din",12),text=" ",fg="red",bg="deepskyblue")
+show_dt.place(x=1380,y=10)
 
 
 show_climate_text=Label(df,font=("Arial",12,"bold"),bg="deepskyblue")
-show_climate_text.place(x=120,y=38)
+show_climate_text.place(x=120,y=25)
 
-climate_info=Label(df,font=("calibari",8,),text="👇Climate Changes🌥️",bg="deepskyblue",fg="black")
-climate_info.place(x=30)
+climate_info=Label(df,font=("calibari",8,),text="👆Show Climate Changes Here👆",bg="deepskyblue",fg="black")
+climate_info.place(x=30,y=62)
 
 show_climate=Label(df,bg="deepskyblue")
-show_climate.place(x=40,y=13)
+show_climate.place(x=40,y=0)
 
 df.mainloop()
